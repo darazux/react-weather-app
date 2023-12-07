643e1e50f6c5e3d0835bd37deacf12fb8678cc62
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-12-07 18:44:03 +0900

    * add Loading animation.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import axios from 'axios';
 import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Results';
+import Loading from './components/Loading';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [city, setCity] = useState('');
   const [results, setResults] = useState({
     country: '',
@@ -17,6 +19,7 @@ function App() {
   });
   const getWeather = (e) => {
     e.preventDefault();
+    setLoading(true);
     axios
       .get(`https://proxy-server-node.vercel.app/weather-data?${city}`)
       .then((res) => {
@@ -28,6 +31,7 @@ function App() {
           icon: res.data.current.condition.icon,
         });
         setCity('');
+        setLoading(false);
       })
       .catch((error) =>
         alert(
@@ -40,7 +44,7 @@ function App() {
       <div className="container">
         <Title />
         <Form setCity={setCity} getWeather={getWeather} city={city} />
-        <Results results={results} />
+        {loading ? <Loading /> : <Results results={results} />}
       </div>
     </div>
   );
